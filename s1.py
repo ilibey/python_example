8295e84aa19ad8d48c9881a60d0f7138a847be69
ilibey <iliya.imani1383tehran.com@gmail.com>
2025-11-07 15:30:02 +0330

    add first file "s1.py"

---
+++ s1.py
@@ -0,0 +1,86 @@
+# متغیر ها در پایتون ------------------------------------
+x ="My age is :"
+y =21
+
+
+# انواع چاپ کردن در پایتون ------------------------------------
+text ="My age is :"
+age =21
+
+print(x,y)
+
+print(x+y)
+
+print(x)
+
+
+#  انواع کامنت ها در پایتون  ------------------------------------
+
+"""
+This is a multiple line comment
+This is a multiple line comment
+This is a multiple line comment
+"""
+
+# this is a comment
+
+# this is a multiple line comment
+# this is a multiple line comment
+# this is a multiple line comment
+
+
+
+# ریخته گری در پایتون ------------------------------------
+intNumber = int(3)
+floatNumber = float(3)
+
+print(intNumber)
+print(floatNumber)
+
+
+
+# دریافت نوع متغیر در پایتون ------------------------------------
+number = 20
+str = "my text"
+
+print(type(number))
+print(type(str))
+
+
+# اختصاص چندین متغیر در پایتون ------------------------------------
+x, y, z = "x", "y", "z"
+print(x)
+print(y)
+print(z)
+
+
+# اختصاص یک مقدار برای چندین متغیر در پایتون ------------------------------------
+x= y = z = "hello"
+print(x)
+print(y)
+print(z)
+
+
+# بازکردن یک مجموعه در پایتون ------------------------------------
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+
+
+# متغیرهای سراسری در پایتون ------------------------------------
+
+x = "awesome"
+def myfunc():
+    print("Python is " + x)
+
+myfunc()
+
+
+
+
+
+
+
